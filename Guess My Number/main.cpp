5cbe678fccfc1f50511953254d9219771a97c09f
Jasmine Hurst <jasmine.ehurst@gmail.com>
2018-04-15 19:00:41 -0700

    Initial Commit

---
+++ Guess My Number/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Guess My Number
+//
+//  Created by Scraggy on 4/15/18.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
