6e9617ddfc497e6fdd04c48250f16069ca28d41b
Jasmine Hurst <jasmine.ehurst@gmail.com>
2018-04-15 19:19:15 -0700

    initial commit

--- Guess My Number/main.cpp
+++ Guess My Number/main.cpp
@@ -6,9 +6,34 @@
 //
 
 #include <iostream>
-
+#include <cstdlib>
+#include <ctime>
+using namespace std;
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
+    
+    srand(static_cast<unsigned int>(time(0)));
+    
+    int secretNumber = rand()% 100 +1;
+    int tries = 0;
+    int guess;
+    int totalTries = 10;
+    
+    while(tries <= totalTries) {
+        cout<<"Enter a guess: \n";
+        cin>>guess;
+        if(guess == secretNumber) {
+            cout<<"You win! \n";
+            return 0;
+        } else if(guess < secretNumber) {
+            cout<<"Too low\n";
+        } else {
+            cout<<"Too high!\n";
+        }
+        tries++;
+    }
+    cout<<"You lose :( \n, the answer was " <<secretNumber;
+    
     return 0;
 }
